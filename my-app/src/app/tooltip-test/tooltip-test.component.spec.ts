a74d3163586fdb67d843ee1ca97c265eed8f2d59
kishorey <kishory@unicourt.com>
2025-03-24 16:14:59 +0530

    changes

---
+++ my-app/src/app/tooltip-test/tooltip-test.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TooltipTestComponent } from './tooltip-test.component';
+
+describe('TooltipTestComponent', () => {
+  let component: TooltipTestComponent;
+  let fixture: ComponentFixture<TooltipTestComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TooltipTestComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(TooltipTestComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
