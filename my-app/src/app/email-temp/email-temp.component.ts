455d43d1594dc62c7f5fd312d9cf55f6ddf55b34
Mispl <kishory@unicourt.com>
2025-08-14 12:29:24 +0530

    updates

---
+++ my-app/src/app/email-temp/email-temp.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-email-temp',
+  templateUrl: './email-temp.component.html',
+  styleUrls: ['./email-temp.component.scss']
+})
+export class EmailTempComponent {
+
+}
