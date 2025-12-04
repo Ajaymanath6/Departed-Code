262e4f4e0fbedf9b06c462f10561859f63a2fdd3
Mispl <kishory@unicourt.com>
2025-12-04 14:49:30 +0530

    ui-changes

--- my-app/src/app/newtheme/newtheme.component.ts
+++ my-app/src/app/newtheme/newtheme.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-newtheme',
   templateUrl: './newtheme.component.html',
-  styleUrls: ['./newtheme.component.scss']
+  styleUrls: ['./newtheme.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class NewthemeComponent {
 
